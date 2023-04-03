1335ae39745a641679705c49ab9aa99c40f01481
Shantanu9799 <cshantanu712@gmail.com>
2023-04-03 10:59:12 +0530

    day1

--- DataTypes.js
+++ DataTypes.js
@@ -1,4 +1,4 @@
-/*
+
 // var - datatype
 var VarName = 'Shantanu';
 //alert(VarName);
@@ -13,30 +13,27 @@ console.log(LetName);
 const ConstName = 'Chatterjee';
 // alert(ConstName);
 console.log(ConstName);
-*/
+
 
 /* Access */
 
 // var can be accessible from everywhere
-// if(true) {
-//     var name = 'Shantanu';
-//     console.log(name);
-// }
-
-// console.log(name);
+if(true) {
+    var name = 'Shantanu';
+    console.log(name);
+}
+console.log(name);
 
 // let can't be
-// if(true) {
-//     let name = 'Shantanu';
-//     console.log(name);
-// }
-
-// console.log(name);
+if(true) {
+    let name = 'Shantanu';
+    console.log(name);
+}
+console.log(name);
 
 // const also
-// if(true) {
-//     const name = 'Shantanu';
-//     console.log(name);
-// }
-
-// console.log(name);
+if(true) {
+    const name = 'Shantanu';
+    console.log(name);
+}
+console.log(name);
---
+++ TemplateString.js
@@ -0,0 +1,9 @@
+// old version 
+let fname = 'Shantanu'
+let lname = 'Chatterjee'
+let age = prompt('Enter my age : ')
+alert(fname + ' ' + lname + ' '+ 'is ' + age + ' years old');
+// console.log(fname + ' ' + lname + ' '+ 'is ' + age + ' years old');
+
+// new version
+alert(`${fname} ${lname} is ${age} years old.`);
