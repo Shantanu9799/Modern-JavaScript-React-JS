cd21ba0caf1b9c32148ff840a6c34628458cf975
Shantanu9799 <cshantanu712@gmail.com>
2023-04-03 11:00:41 +0530

    slite changes

--- TemplateString.js
+++ TemplateString.js
@@ -7,3 +7,5 @@ alert(fname + ' ' + lname + ' '+ 'is ' + age + ' years old');
 
 // new version
 alert(`${fname} ${lname} is ${age} years old.`);
+
+// while using ES6 to concate string we can use the ``. for accessing variables use the ${Variable}
