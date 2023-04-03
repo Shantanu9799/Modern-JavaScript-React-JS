f8b66581c1fc7872418cfdc0000690eeff670699
Shantanu9799 <cshantanu712@gmail.com>
2023-04-03 10:52:32 +0530

    day1

---
+++ DataTypes.js
@@ -0,0 +1,42 @@
+/*
+// var - datatype
+var VarName = 'Shantanu';
+//alert(VarName);
+console.log(VarName);
+
+// let - datatype
+let LetName = 'Santu'
+// alert(LetName);
+console.log(LetName);
+
+// const - datatpye
+const ConstName = 'Chatterjee';
+// alert(ConstName);
+console.log(ConstName);
+*/
+
+/* Access */
+
+// var can be accessible from everywhere
+// if(true) {
+//     var name = 'Shantanu';
+//     console.log(name);
+// }
+
+// console.log(name);
+
+// let can't be
+// if(true) {
+//     let name = 'Shantanu';
+//     console.log(name);
+// }
+
+// console.log(name);
+
+// const also
+// if(true) {
+//     const name = 'Shantanu';
+//     console.log(name);
+// }
+
+// console.log(name);
