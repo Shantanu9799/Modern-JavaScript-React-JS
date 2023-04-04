c01499c9f428e0670ff4e39508a9018c7a1de655
Shantanu9799 <cshantanu712@gmail.com>
2023-04-04 11:59:33 +0530

    day2

---
+++ Arraydestructuring.js
@@ -0,0 +1,6 @@
+// Array destructuring
+let [firstName] = ["Shantanu", "Poly", "Buddhadip", "Rani", "Chandan"];
+console.log(firstName);
+//with , - separator we can select the position in an array
+let [, secondName] = ["Shantanu", "Poly", "Buddhadip", "Rani", "Chandan"];
+console.log(secondName);
