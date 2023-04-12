e982a94bbb44d7d8944d9775172dc8c0cd9f56a3
Shantanu9799 <cshantanu712@gmail.com>
2023-04-12 11:41:39 +0530

    day2.. Conditional checking

--- ReactJS/youtube/src/App.js
+++ ReactJS/youtube/src/App.js
@@ -1,13 +1,34 @@
-import "./App.css"
+import "./App.css";
 import Video from "./component/Video";
 
 function App() {
+  //we can make use of obj to pass all the information;
+  let obj = {
+    source:
+      "https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124",
+    title: "React JS Tutorial",
+    views: "100K",
+    time: "1 year",
+    verified : false,
+  };
   return (
     //also we can make a object of all the properties and directly put it like <Video {...obj} />.
     <div className="App">
-      <Video source = "https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124" title="React JS Tutorial" views="100K" time="1 year" />
-      <Video source = "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo.jpg" title="JAVA Tutorial"  views="10M" time="2 months" />
-      <Video source = "https://www.freecodecamp.org/news/content/images/2021/07/mysql.png" title="MySql Tutorial"  views="10K" time="11 months" />
+      <Video {... obj}/>
+      <Video
+      verified = {true}
+        source="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo.jpg"
+        title="JAVA Tutorial"
+        views="10M"
+        time="2 months"
+      />
+      <Video
+        source="https://www.freecodecamp.org/news/content/images/2021/07/mysql.png"
+        title="MySql Tutorial"
+        views="10K"
+        time="11 months"
+        verified = {false}
+      />
     </div>
   );
 }
--- ReactJS/youtube/src/component/Video.js
+++ ReactJS/youtube/src/component/Video.js
@@ -1,13 +1,19 @@
-import "./Video.css";
+import "./Video.css";  // importing css file
 
-function Video({ source, title, channel = "santu", views, time }) {
+function Video({ source, title, channel = "Santu", views, time, verified = true }) { // de-structuring the object and used them as a variable
+  let channelJSX;
+  if(verified) {
+    channelJSX = <div className="channelName">{channel}☑️</div>
+  } else {
+    channelJSX = <div className="channelName">{channel}</div>
+  }
   return (
     <div className="container">
       <div className="pic">
         <img src={source} alt="Opps!!" />
       </div>
       <div className="title">{title}</div>
-      <div className="channelName">{channel}</div>
+      {channelJSX}
       <div className="details">
         {views} views <span>.</span> {time} ago
       </div>
