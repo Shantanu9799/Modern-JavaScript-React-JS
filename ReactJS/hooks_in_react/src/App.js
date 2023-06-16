6722cfdf41692b708114e36df46485dfeffce07f
Shantanu9799 <cshantanu712@gmail.com>
2023-06-16 12:05:42 +0530

    Use State Hook in react

--- ReactJS/hooks_in_react/src/App.js
+++ ReactJS/hooks_in_react/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Count from "./Hooks/UseEffect/Count.js";
+import Object from "./Hooks/UseEffect/Object.js";
 
 function App() {
   return (
     <div className="App">
-      
+      <Count />
+      <Object />
     </div>
   );
 }
