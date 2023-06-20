dc756a0bdf574cf4ca52c6b83bb6af00357b4448
Shantanu9799 <cshantanu712@gmail.com>
2023-06-20 15:30:42 +0530

    UseReducer Hook done

--- ReactJS/hooks_in_react/src/App.js
+++ ReactJS/hooks_in_react/src/App.js
@@ -4,6 +4,7 @@ import Count from "./Hooks/UseState/Count.js";
 import Object from "./Hooks/UseState/Object.js";
 import TitleUpdate from "./Hooks/UseEffect/TitleUpdate";
 import ScreenWidth from "./Hooks/UseEffect/ScreenWidth";
+import Counter from "./Hooks/UseReducer/Counter"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Object />
       <TitleUpdate />
       <ScreenWidth />
+      <Counter />
     </div>
   );
 }
