80add1ec35f1be8497edba9d8b6437d2a1b92af3
Shantanu9799 <cshantanu712@gmail.com>
2023-06-21 12:38:58 +0530

    footer created

--- ReactJS/my-portfolio/src/routes/Home.js
+++ ReactJS/my-portfolio/src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import NavBar from '../Components/NavBar'
 import HeroImg from '../Components/HeroImg'
+import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
     <>
     <NavBar />
     <HeroImg />
+    <Footer />
     </>
   )
 }
