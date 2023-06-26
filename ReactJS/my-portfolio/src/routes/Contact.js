48162a35ba0700419505c1ddcacf61377fd00b96
Shantanu9799 <cshantanu712@gmail.com>
2023-06-26 11:26:18 +0530

    Video part done

--- ReactJS/my-portfolio/src/routes/Contact.js
+++ ReactJS/my-portfolio/src/routes/Contact.js
@@ -3,12 +3,14 @@ import React from "react";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
 import HeroImg2 from "../Components/HeroImg2";
+import Form from "../Components/Form";
 
 const Contact = () => {
   return (
     <div>
       <NavBar />
       <HeroImg2 heading="CONTACTS" text="Let's have a chat" />
+      <Form />
       <Footer />
     </div>
   );
