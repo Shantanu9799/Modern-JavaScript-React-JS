b08d7fed30d49e4db82008aabc34c84453a5fc2f
Shantanu9799 <cshantanu712@gmail.com>
2023-07-24 14:58:00 +0530

    edit functionality added

--- ReactJS with Typescript/notes-app/src/App.tsx
+++ ReactJS with Typescript/notes-app/src/App.tsx
@@ -9,12 +9,26 @@ function App() {
 
   const [notes, setNotes] = useState(Notes);
 
+  const [editMode, setEditMode] = useState(false);
+  const [noteToBeEditted, setNoteToBeEditted] = useState<NoteType | null>(null);
+
   const addNote = (note: NoteType) => {
     setNotes([note,...notes]);
   }
 
+  const updateNote = (updatedNote: NoteType) => {
+    const index = notes.findIndex(note => note.id === updatedNote.id);
+    const editedNotes = [...notes];
+    editedNotes.splice(index, 1, updatedNote);
+    setNotes(editedNotes);
+  }
+
   const editNote = (id: string) => {
-    console.log('edited', id);
+    setEditMode(true);
+    const editableNote = notes.find(note => note.id === id);
+    if(editableNote){
+      setNoteToBeEditted(editableNote);
+    }
   }
 
   const deleteNote = (id: string) => {
@@ -27,7 +41,7 @@ function App() {
   return (
     <div className="App">
       <h2>Notes app using React with typescript</h2>
-      <AddNote addNote = { addNote }/>
+      <AddNote addNote = { addNote } editMode={editMode} noteToBeEditted={noteToBeEditted} updateNote={updateNote} />
       <div>
         {
           notes.map((note) => <Note key={ note.id } id={ note.id } text={ note.text } priority={ note.priority } editNote={ editNote } deleteNote={ deleteNote } />)
--- ReactJS with Typescript/notes-app/src/components/add-note/add-note.tsx
+++ ReactJS with Typescript/notes-app/src/components/add-note/add-note.tsx
@@ -1,49 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./add-note.css";
-import { NoteType, Priority } from '../note/note-type';
-import {v4 as uuidv4} from "uuid";
+import { NoteType, Priority } from "../note/note-type";
+import { v4 as uuidv4 } from "uuid";
 import { Card } from "../card/card";
 
 type addNotePops = {
-  addNote : (note : NoteType) => void;
-}
+  addNote: (note: NoteType) => void;
+  editMode: boolean;
+  noteToBeEditted: NoteType | null;
+  updateNote: (updatedNote: NoteType) => void;
+};
 
-function AddNote(props : addNotePops) {
-
+function AddNote(props: addNotePops) {
   const [text, setText] = useState("");
 
   const [priority, setPriority] = useState<Priority>("low");
 
-  const handelChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
-  }
+  };
+
+  const setNoteContent = (note: NoteType) => {
+    setText(note.text);
+    setPriority(note.priority);
+  };
+
+  useEffect(() => {
+    if (props.noteToBeEditted) {
+      setNoteContent(props.noteToBeEditted);
+    }
+  }, [props.editMode, props.noteToBeEditted]);
 
   const handelClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    props.addNote({
-      text,
-      priority,
-      id:uuidv4(),
-    })
-    setText('');
-    setPriority('low');
+    if (props.editMode) {
+      props.noteToBeEditted && props.updateNote({
+        text,
+        priority,
+        id: props.noteToBeEditted.id,
+      });
+    } else {
+      props.addNote({
+        text,
+        priority,
+        id: uuidv4(),
+      });
+    }
+
+    setText("");
+    setPriority("low");
   };
 
   const handelSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setPriority(e.target.value as Priority);
-  }
-  
+  };
 
   return (
     <Card bgColor="rgb(12,21,122)" height="5" padding="2">
       <form className="add-note">
-        <input type="text" onChange={handelChange} value={text} placeholder="Enter your card name here" />
+        <input
+          type="text"
+          onChange={handelChange}
+          value={text}
+          placeholder="Enter your card name here"
+        />
         <select onChange={handelSelect} value={priority}>
           <option value="high">High</option>
           <option value="medium">Medium</option>
           <option value="low">Low</option>
         </select>
-        <button onClick={handelClick}>ADD</button>
+        <button onClick={handelClick}>
+          {props.editMode ? "UPDATE" : "ADD"}
+        </button>
       </form>
     </Card>
   );
