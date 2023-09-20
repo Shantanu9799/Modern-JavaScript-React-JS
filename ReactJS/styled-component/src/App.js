f720203b01645b3b86f19fb2a1e915a01c9765c0
Shantanu9799 <cshantanu712@gmail.com>
2023-09-20 12:39:58 +0530

    api call with image carousel

--- ReactJS/styled-component/src/App.js
+++ ReactJS/styled-component/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Greet from './Components/Greet';
 
+
 function App() {
   return (
     <div className="App">
--- ReactJS/styled-component/src/Components/Greet.js
+++ ReactJS/styled-component/src/Components/Greet.js
@@ -22,3 +22,6 @@ const greet = () => {
 }
 
 export default greet;
+
+
+
