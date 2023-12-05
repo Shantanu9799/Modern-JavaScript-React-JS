45d678c9f47106d22eb45b414de9956ba72e22d7
Shantanu9799 <cshantanu712@gmail.com>
2023-12-05 11:30:45 +0530

    react with vite

---
+++ ReactJS/react-vite-project01/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
